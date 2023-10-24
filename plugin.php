facddf409249c9d73ab90df2e093886a292b138f
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:07:33 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_equivalent_fractions_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Equivalent Fractions Calculator by Calculator.iO";
 
 function display_ci_equivalent_fractions_calculator(){
